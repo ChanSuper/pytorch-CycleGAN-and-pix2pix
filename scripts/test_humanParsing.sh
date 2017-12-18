686e6f28457b405f65ea1aa7de449694ad62b1f8
chenchao <1429535561@qq.com>
2017-12-18 20:19:14 +0800

    debug

--- scripts/test_humanParsing.sh
+++ scripts/test_humanParsing.sh
@@ -1,1 +1,1 @@
-python train.py --dataroot ./datasets/humanparsing --name humanparsing_pix2pix --model pix2pix --which_model_netG unet_256 --which_direction AtoB --lambda_A 100 --dataset_mode parsing --no_lsgan --norm batch --pool_size 0 --no_flip --output_nc 1 --parts 20
+python train.py --dataroot ./datasets/humanparsing --name humanparsing_pix2pix --model pix2pix --which_model_netG unet_256 --which_direction AtoB --lambda_A 100 --dataset_mode parsing --no_lsgan --norm batch --pool_size 0 --no_flip --output_nc 20 --parts 20
