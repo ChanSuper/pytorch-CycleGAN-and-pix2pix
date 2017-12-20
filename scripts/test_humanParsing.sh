5735f11c953bd9ee987b2e855d2c5057b143c1da
chenchao <1429535561@qq.com>
2017-12-20 23:17:54 +0800

    lambda_A 1.5

--- scripts/test_humanParsing.sh
+++ scripts/test_humanParsing.sh
@@ -1,1 +1,1 @@
-python train.py --dataroot ./datasets/humanparsing --name humanparsing_pix2pix --model pix2pix --which_model_netG unet_256 --which_direction AtoB --lambda_A 10 --dataset_mode parsing --no_lsgan --norm batch --pool_size 0 --no_flip --output_nc 20 --parts 20
+python train.py --dataroot ./datasets/humanparsing --name humanparsing_pix2pix --model pix2pix --which_model_netG unet_256 --which_direction AtoB --lambda_A 1.5 --dataset_mode parsing --no_lsgan --norm batch --pool_size 0 --no_flip --output_nc 20 --parts 20
